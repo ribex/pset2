2e02429007e3976b2c0f4415371b49b3a4709b05
Rebecca Wagaman <ribexy@gmail.com>
2017-10-29 14:32:10 +0000

    Placeholders for plaintext and ciphertext

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -9,12 +9,21 @@ int main(int argc, string argv[]) {
         printf("Usage: ./caesar k\n");
         return 1;
     }
-    printf("%s\n", argv[1]);
 
-    int k = atoi(argv[1]);
+    // printf("%s\n", argv[1]);
 
-    int key = k % 26;
+    int k = (atoi(argv[1])) % 26;
 
-    printf("key is %i\n", key);
+    printf("key is %i\n", k);
+
+    printf("plaintext: ");
+
+    string plaintext = get_string();
+
+    printf("this was %s\n", plaintext);
+
+    string ciphertext = "blah";
+
+    printf("ciphertext: %s\n", ciphertext);
 
 }
