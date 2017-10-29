dadfa457dc037033fdfb638cd6c2e93bc10a447e
Rebecca Wagaman <ribexy@gmail.com>
2017-10-29 02:11:06 +0000

    Key is integer

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,11 +3,18 @@
 
 int main(int argc, string argv[]) {
 
+    // check number of arguments
     if (argc != 2) {
+        // inform user of proper usage
         printf("Usage: ./caesar k\n");
         return 1;
     }
-
     printf("%s\n", argv[1]);
+
+    int k = atoi(argv[1]);
+
+    int key = k % 26;
+
+    printf("key is %i\n", key);
 
 }
